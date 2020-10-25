ce9ebc6770d83493daa9fda57d302e978afea9a3
KondalDurgam <kondaldurgam733@gmail.com>
2020-10-25 13:57:25 +0530

    React Sample task

--- Day1/task3/react-tutorial/src/App.js
+++ Day1/task3/react-tutorial/src/App.js
@@ -1,32 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-	render() {
-			return (
-			<div className = "App">
-					<h1>Hi, am Creating React Js App</h1>
-					<h1>
-						Another h1 Tag is  created
-					</h1>
-					<Person/>
-			</div>
-			
-			)
-			// return React.createElement('div',{className : 'App'},null,React.createElement('h1',{className:'text-color'},null,'Hey Am Creating App!!!!'))
+const App = props => {
+	const [personState, setPersonState] = useState({
+		person: [
+			{ name: "Kondal", age: "26" },
+			{ name: "Ammulu", age: "15" },
+			{ name: "Kanna", age: "25" },
+			{ name: "Anu", age: "17" }
+		],
+		OtherState: 'SomeOtherValue'
+	});
+
+	const [OtherState ,setOtherState] = useState("Some Other Value");
+	console.log(personState,OtherState);
+	const switchToHandler = () => {
+		// this.state.person[0] = "James Dawson" Never ever Dont do this;
+		setPersonState({
+			person: [
+				{ name: "Ravi", age: "26" },
+				{ name: "Ammulu", age: "15" },
+				{ name: "Kanna", age: "25" },
+				{ name: "Himnhu", age: "3222" }
+			]
+		})
 	}
+	return (
+		<div className="App">
+			<h1>
+				Hi, am Creating React Js App
+			</h1>
+			<h1>
+				Another h1 Tag is  created
+			</h1>
+			<button onClick={switchToHandler} >Switch button</button>
+			<Person name={personState.person[0].name} age={personState.person[0].age} />
+			<Person name={personState.person[1].name} age={personState.person[1].age} />
+			<Person name={personState.person[2].name} age={personState.person[2].age}>my Hobbies are Playing fireFree</Person>
+			<Person name={personState.person[3].name} age={personState.person[3].age} />
+		</div>
+
+	)
 
 }
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hi Kondal</h1>
-//     </div>
-//   );
-// }
 
 export default App;
+
--- Day1/task3/react-tutorial/src/Person/Person.js
+++ Day1/task3/react-tutorial/src/Person/Person.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p>Hey Umm Am Perosn</p>
-}
+const person = (props) => {
+    return (
+        <div>
+            <p>Iam the person {props.name} Iam {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    )
+
+};
 export default person;
